19ecc0cc449941db160d7f40825c3b936fd409f7
Veda Vadyar <vvadyar@andrew.cmu.edu>
2012-04-25 20:32:16 -0400

    Finished models for article and category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ActiveRecord::Base
+  # Relationships
   has_many :photos
+  has_many :articles
   
   scope :active, where('active = ?', true)
   scope :alphabetical, order('name')
